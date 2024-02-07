843de535770529238649d700bba5999407fe1692
jintao liu <jintao.liu@amazingsys.com>
2024-02-07 16:35:39 +0800

    添加api文档

--- genApi.sh
+++ genApi.sh
@@ -1,3 +1,5 @@
-goctl api go --api ./apps/user/user.api --dir ./apps/user
+goctl api  go --api ./apps/user/user.api --dir ./apps/user
 
 goctl api go --api ./apps/message/message.api --dir ./apps/message
+
+goctl api plugin -plugin goctl-swagger="swagger -filename user-api.json -host 127.0.0.1:6001" --api ./apps/user/user.api --dir ./docs
